0c2c5cf290f0ba677defbd78a773997255a1350b
alanclarke <hi@alz.so>
2016-04-01 13:07:06 +0100

    tweak defaults

--- overlayer.js
+++ overlayer.js
@@ -5,16 +5,18 @@ module.exports = function overlayer (opts) {
   if (typeof opts.speed === 'undefined') opts.speed = 1000
   var cleanups = []
   var el = document.createElement('div')
-  var style = 'position:fixed;width:100%;height:100%;top:0px;left:0px;background:rgba(0,0,0,0.7);z-index:1;'
+  var style = 'position:fixed;width:100%;height:100%;top:0px;left:0px;background:rgba(20,20,20,0.8);z-index:1;'
   el.setAttribute('style', style)
   if (opts.auto !== false) show()
   return { el: el, show: show, hide: hide, destroy: destroy }
 
   function show (speed) {
+    el.style.opacity = 0
     fade(1, speed)
   }
 
   function hide (speed) {
+    el.style.opacity = 1
     fade(0, speed)
   }
 
